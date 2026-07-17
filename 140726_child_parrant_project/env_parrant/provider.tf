f8fd4cb32eaabe811d234c9693b81ed240f06ebf
Vikas Belekar <belekarvikas@gmail.com>
2026-07-17 19:18:19 +0530

    Add Azure backend configuration to provider.tf
    Configure backend for Azure storage in provider.tf

--- 140726_child_parrant_project/env_parrant/provider.tf
+++ 140726_child_parrant_project/env_parrant/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.80.0"
     }
   }
+backend "azurerm"{
+    resource_group_name   = "b18g35-common-rg"          
+    storage_account_name  = "b18g35storageaccount"     
+    container_name        = "vikasado"  
+    key                   = ""
+
+}
 }
 provider "azurerm" {
   features {}
